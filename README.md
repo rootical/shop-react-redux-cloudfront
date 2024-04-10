864a94e4618d9ba43f17fc6eaf08f860b1416a46
Vadzim Yakushau <vadzim_yakushau@epam.com>
2024-04-10 19:05:46 +0200

    feat(bfe): add API

--- README.md
+++ README.md
@@ -1,24 +1,22 @@
 # React-shop-cloudfront
 
-## [Cloudfront](https://d1mnmbjxzsulwe.cloudfront.net/)
-
-## [S3](https://s3-cloudfront-shop.s3.eu-central-1.amazonaws.com/index.html)
+[CloudFront Link](https://d1mnmbjxzsulwe.cloudfront.net/)
 
 This is frontend starter project for nodejs-aws mentoring program. It uses the following technologies:
 
--   [Vite](https://vitejs.dev/) as a project bundler
--   [React](https://beta.reactjs.org/) as a frontend framework
--   [React-router-dom](https://reactrouterdotcom.fly.dev/) as a routing library
--   [MUI](https://mui.com/) as a UI framework
--   [React-query](https://react-query-v3.tanstack.com/) as a data fetching library
--   [Formik](https://formik.org/) as a form library
--   [Yup](https://github.com/jquense/yup) as a validation schema
--   [Serverless](https://serverless.com/) as a serverless framework
--   [Vitest](https://vitest.dev/) as a test runner
--   [MSW](https://mswjs.io/) as an API mocking library
--   [Eslint](https://eslint.org/) as a code linting tool
--   [Prettier](https://prettier.io/) as a code formatting tool
--   [TypeScript](https://www.typescriptlang.org/) as a type checking tool
+- [Vite](https://vitejs.dev/) as a project bundler
+- [React](https://beta.reactjs.org/) as a frontend framework
+- [React-router-dom](https://reactrouterdotcom.fly.dev/) as a routing library
+- [MUI](https://mui.com/) as a UI framework
+- [React-query](https://react-query-v3.tanstack.com/) as a data fetching library
+- [Formik](https://formik.org/) as a form library
+- [Yup](https://github.com/jquense/yup) as a validation schema
+- [Serverless](https://serverless.com/) as a serverless framework
+- [Vitest](https://vitest.dev/) as a test runner
+- [MSW](https://mswjs.io/) as an API mocking library
+- [Eslint](https://eslint.org/) as a code linting tool
+- [Prettier](https://prettier.io/) as a code formatting tool
+- [TypeScript](https://www.typescriptlang.org/) as a type checking tool
 
 ## Available Scripts
 
--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -6,50 +6,43 @@ import Header from "~/components/MainLayout/components/Header";
 import Box from "@mui/material/Box";
 
 function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {"Copyright © "}
-            <Link
-                color="inherit"
-                href="https://material-ui.com/"
-                underline="hover"
-            >
-                Cloudfront Store
-            </Link>{" "}
-            {new Date().getFullYear()}
-            {"."}
-        </Typography>
-    );
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/" underline="hover">
+        Blanket Store
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
 const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    return (
-        <>
-            <Header />
-            <main>
-                <Container sx={{ pb: 8 }} maxWidth="md">
-                    {children}
-                </Container>
-            </main>
-            <Box
-                component={"footer"}
-                sx={{
-                    bgcolor: (theme) => theme.palette.background.paper,
-                    padding: 6,
-                }}
-            >
-                <Typography
-                    variant="subtitle1"
-                    align="center"
-                    color="textSecondary"
-                    component="p"
-                >
-                    Thank you for your purchase!
-                </Typography>
-                <Copyright />
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <main>
+        <Container sx={{ pb: 8 }} maxWidth="md">
+          {children}
+        </Container>
+      </main>
+      <Box
+        component={"footer"}
+        sx={{ bgcolor: (theme) => theme.palette.background.paper, padding: 6 }}
+      >
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          Thank you for your purchase!
+        </Typography>
+        <Copyright />
+      </Box>
+    </>
+  );
 };
 
 export default MainLayout;
