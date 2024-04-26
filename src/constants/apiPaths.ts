1d8618721bce8fbbc01997499bfb5dc7e9934583
Vadzim Yakushau <vadzim_yakushau@epam.com>
2024-04-26 15:37:29 +0200

    feat: task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://ht9wjv9ht5.execute-api.eu-central-1.amazonaws.com/dev",
+  order: "https://ht9wjv9ht5.execute-api.eu-central-1.amazonaws.com/dev",
+  import: "https://wgb9i0235k.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://ht9wjv9ht5.execute-api.eu-central-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://ht9wjv9ht5.execute-api.eu-central-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
